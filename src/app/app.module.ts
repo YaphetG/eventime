4b49b463c40aa68e0b0c4788882f7196e5e63ff1
Gobi <yaphet.gobenak@gmail.com>
2017-09-03 18:40:56 -0500

    Directory import fixed linux case sensetive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,20 @@ import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
-import { SearchFormComponent } from './Components/search-form/search-form.component';
-import { PublicEventComponent } from './Components/public-event/public-event.component';
-import { HomeComponent } from './Components/home/home.component';
-import { AddEventComponent } from './Components/add-event/add-event.component';
-import { CommentComponent } from './Components/comment/comment.component';
-import { ProfileComponent } from './Components/profile/profile.component';
-import { ActiveEventsComponent } from './Components/active-events/active-events.component';
-import { EventDetailComponent } from './Components/event-detail/event-detail.component';
-import { MyEventsComponent } from './Components/my-events/my-events.component';
-import { MyActivityComponent } from './Components/my-activity/my-activity.component';
-import { LoginComponent } from './Components/login/login.component';
-import { LogoutComponent } from './Components/logout/logout.component';
-import { PreviousEventsComponent } from './Components/previous-events/previous-events.component';
-import { MainLayoutComponent } from './Components/main-layout/main-layout.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
+import { PublicEventComponent } from './components/public-event/public-event.component';
+import { HomeComponent } from './components/home/home.component';
+import { AddEventComponent } from './components/add-event/add-event.component';
+import { CommentComponent } from './components/comment/comment.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ActiveEventsComponent } from './components/active-events/active-events.component';
+import { EventDetailComponent } from './components/event-detail/event-detail.component';
+import { MyEventsComponent } from './components/my-events/my-events.component';
+import { MyActivityComponent } from './components/my-activity/my-activity.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { PreviousEventsComponent } from './components/previous-events/previous-events.component';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
