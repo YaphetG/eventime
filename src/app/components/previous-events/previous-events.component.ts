0f6e4a38998a2c04a4f0dd84b31109dbc25d51fb
Brhane <brhane.abrham87@gmail.com>
2017-09-03 17:45:32 -0500

    add components

---
+++ src/app/components/previous-events/previous-events.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-previous-events',
+  templateUrl: './previous-events.component.html',
+  styleUrls: ['./previous-events.component.css']
+})
+export class PreviousEventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
