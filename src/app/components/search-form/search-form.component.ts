44f16a474da742783d655432be3472acc9eaef94
Gobi <yaphet.gobenak@gmail.com>
2017-09-03 21:46:56 -0500

    Search form partially complete, going for mafi's base

--- src/app/components/search-form/search-form.component.ts
+++ src/app/components/search-form/search-form.component.ts
@@ -1,19 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,NgModule } from '@angular/core';
+import { SearchFilter } from "./search.filter";
+
+
+@NgModule({
+  declarations:[
+    SearchFilter
+  ]
+})
 
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
   styleUrls: ['./search-form.component.css']
 })
+
 export class SearchFormComponent implements OnInit {
   options = [
     'opt1',
     'opt2',
     'opt3',
   ];
+  query : string;
   constructor() { }
 
   ngOnInit() { }
 
+  search(){
+    console.log("search enter");
+  }
 
 }
